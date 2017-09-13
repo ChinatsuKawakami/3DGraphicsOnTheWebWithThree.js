107ba766e5ba9596cf1ec8da66e24ad6023d9846
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2017-09-13 15:57:34 -0400

    0.0.2 add console.log(THREE) in main.js file

---
+++ Ex_Files_3D_Graphics_Web_ThreeJS/Exercise Files/chapter-01/01_02/begin/main.js
@@ -0,0 +1,9 @@
+/*
+Author : Engine Arslan
+Editor&Learner : Chinatsu Kawakami
+Download Date: 2017-09-13
+Latest Edit: 2017-09-13
+Description : This is the main.js file to add functions for creating 3D Graphics with Three.js
+*/
+
+console.log(THREE);
