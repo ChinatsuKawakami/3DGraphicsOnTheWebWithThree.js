ea8ebdc1e69bcdcffdfe19008e6082d0942d586b
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2017-09-14 12:33:58 -0400

    0.0.5 copy and paste lib and assets files into \Exercise Files\chapter-01\01_04\begin\ then add the code to print 3D box in index.html

--- Ex_Files_3D_Graphics_Web_ThreeJS/Exercise Files/chapter-01/01_04/begin/main.js
+++ Ex_Files_3D_Graphics_Web_ThreeJS/Exercise Files/chapter-01/01_04/begin/main.js
@@ -1,12 +1,30 @@
+/*
+Author : Engine Arslan
+Editor&Learner : Chinatsu Kawakami
+Download Date: 2017-09-13
+Latest Edit: 2017-09-14
+Description : This is the main.js file to add functions for creating 3D Graphics with Three.js
+*/
+
+
 function init() {
 	var scene = new THREE.Scene();
+    
+    var box = getBox(1,1,1);
 
+    scene.add(box);
 	var camera = new THREE.PerspectiveCamera(
 		45,
 		window.innerWidth/window.innerHeight,
 		1,
 		1000
 	);
+	camera.position.x = 1;
+	camera.position.y = 2;
+	camera.position.z = 5;
+    
+    camera.lookAt(new THRiEE.Vector3(0,0,0));
+
 	var renderer = new THREE.WebGLRenderer();
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	document.getElementById('webgl').appendChild(renderer.domElement);
@@ -14,6 +32,19 @@ function init() {
 		scene,
 		camera 
 	);
+}
+
+function getBox(w,h,d){
+	var geometry = new THREE.BoxGeometry(1,1,1);
+    var material = new THREE.MeshBasicMaterial({
+    	color:0x00ff00
+    });
+    var mesh = new THREE.Mesh(
+    	geometry,
+    	material
+    	);
+
+    return mesh;
 }
 
 init();
